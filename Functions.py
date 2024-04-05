f06fdcfe4a98e19357058790a2b4c8baf270d9a9
hercules1408 <mr.sanjaysinghyadav2@gmail.com>
2024-04-05 17:10:38 +0530

    first

--- Functions.py
+++ Functions.py
@@ -1,9 +1,16 @@
-def get_todos(filepath='Files/Subfiles/todos.txt'):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath='Files/Subfiles/todos.txt'):
-    with open(filepath, 'w') as file_write:
-        file_write.writelines(todos_arg)
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
